54754d0e775192e7c7c1fd297704a4d8fba75d66
Tom G <tomg@byu.net>
2019-01-24 22:32:34 -0500

    PHP 7.1
    Also added phpstan check/fixes.

--- src/IuCasAuthentication.php
+++ src/IuCasAuthentication.php
@@ -10,99 +10,67 @@
  * The three necessary URLs (login, validation, logout) as described by https://kb.iu.edu/d/atfc are
  * included by default but may be overridden through the use of the following optional environment variables.
  *
- * * `CAS_LOGIN_URL` defaults to `'https://cas.iu.edu/cas/login'`
+ * * `CAS_LOGIN_URL` defaults to ``
  * * `CAS_VALIDATION_URL` defaults to `'https://cas.iu.edu/cas/validate'`
  * * `CAS_LOGOUT_URL` defaults to `'https://cas.iu.edu/cas/logout'`
+ *
+ * Two additional environment variables are used:
+ *
+ * * `CAS_SESSION_VAR` defaults to `'CAS_USER'`, therefore the user name of the active user is available at
+ *   `$_SESSION[getenv('CAS_SESSION_VAR')]`. Current value is returned by `#getSessionVar()`. The session variable is
+ *    used by `#getUserName()` and `#setUserName()`, which in turn are used by `#logout()` and the default success
+ *    handler for `#authenticate()` to store the user name response from CAS in `$_SESSION`.
+ * * `CAS_TIMEOUT` Sets the timeout of service requests to CAS validation and logout services. Defaults to `5`.
  */
 
 namespace IuCas;
 
 class IuCasAuthentication
 {
-    protected $service;
-    protected $redirectUrl;
+    public const CAS_DEFAULT_TIMEOUT = 5; // Preference given to CAS_TIMEOUT environment var
     
-    protected $logger;
+    public const CAS_LOGIN_URL = 'https://cas.iu.edu/cas/login'; // Preference given to environment var
+    public const CAS_VALIDATION_URL = 'https://cas.iu.edu/cas/validate'; // Preference given to environment var
+    public const CAS_LOGOUT_URL = 'https://cas.iu.edu/cas/logout'; // Preference given to environment var
     
-    protected $userName;
+    public const CAS_SESSION_VAR = 'CAS_USER';
+    
+    protected $timeout;
     
     /**
-     * An overridable callable, so exit on failed auth/redirect can be tested.
+     * @var string Immutable after creation
      */
-    protected $exitCallable;
+    protected $service = 'IU';
+    
+    /**
+     * @var string
+     */
+    protected $redirectUrl;
+    
+    /**
+     * @var null|\Psr\Log\LoggerInterface Reference to a PSR-4 Logger
+     */
+    protected $logger = null;
+    
+    /**
+     * @var null|string
+     */
+    protected $userName = null;
     
     /**
      * @param string $redirect URL for your application that should be redirected to for validation after
      *        authentication. Must be the same during authentication and validation steps.
      * @param string $service (optional) Must be one of the [CAS application codes at IU](https://kb.iu.edu/d/alqm).
      *        Defaults to 'IU'
-     * @param LoggerInterface $logger (optional) A reference to a PSR-3 logger (such as Monolog).
+     * @param \Psr\Log\LoggerInterface $logger (optional) A reference to a PSR-3 logger (such as Monolog).
      *        Used to log validation errors, if available.
      */
-    public function __construct($redirect = '', $service = "IU", $logger = null)
+    public function __construct(?string $redirect = null, string $service = 'IU', $logger = null)
     {
-        
         $this->service = $service;
-        $this->redirectUrl = $redirect ? $redirect : $this->getCurrentUrl();
+        $this->redirectUrl = $redirect ?? $this->getCurrentUrl();
         
-        $this->setLogger($logger ? $logger : null);
-        
-        $this->setExit(function () {
-            exit;
-        }); // Default. May be overridden easily
-    }
-    
-    /**
-     * @param string $redirect URL that authentication should redirect to after authentication.
-     *                         Must be the same during authentication and validation steps.
-     *
-     */
-    public function getService()
-    {
-        return $this->service;
-    }
-    
-    public function getRedirectUrl()
-    {
-        return $this->redirectUrl;
-    }
-    
-    public function setRedirectUrl($url)
-    {
-        $this->redirectUrl = $url;
-    }
-    
-    public function getCasLoginUrl()
-    {
-        $url = getenv('CAS_LOGIN_URL');
-        $url = $url ? $url : 'https://cas.iu.edu/cas/login';
-        
-        return $url
-            . "?cassvc={$this->service}&casurl="
-            . rawurlencode($this->redirectUrl);
-    }
-    
-    public function getCasTicket()
-    {
-        return isset($_GET['casticket']) ? $_GET['casticket'] : '';
-    }
-    
-    public function getCasValidationUrl()
-    {
-        // Validation **will** fail if $_GET['casticket'] is empty.
-        // A CAS ticket may be used only once.
-        $base = getenv('CAS_VALIDATION_URL');
-        $base = $base ? $base : 'https://cas.iu.edu/cas/validate';
-        
-        return $base
-            . "?cassvc={$this->service}&casurl="
-            . rawurlencode($this->redirectUrl)
-            . "&casticket=" . $this->getCasTicket();
-    }
-    
-    public function setLogger($logger)
-    {
-        $this->logger = $logger;
+        $this->setLogger($logger);
     }
     
     /**
@@ -113,26 +81,27 @@ class IuCasAuthentication
      *                     Set to zero for no timeout
      * @return string|null Returns CAS username, or null if validation failed.
      */
-    public function validate($timeout = 5)
+    public function validate(?int $timeout = null) : ?string
     {
+        
         // CAS sends response on 2 lines. First line contains "yes" or "no".
         // If "yes", second line contains username (otherwise, it is empty).
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $this->getCasValidationUrl());
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout ?? $this->getTimeout());
         ob_start();
         curl_exec($curl);
 
         if ($errNum = curl_errno($curl)) {
             if ($this->logger) {
-                $this->logger->critical(__CLASS__ . ': ' . curl_err($curl));
+                $this->logger->critical(__CLASS__ . ': ' . curl_error($curl));
                 
-                $this->logger->critical(__CLASS__ . ": Validation request to {$this->casValidationUrl} timed out.");
+                $this->logger->critical(__CLASS__ . ": Validation request {$this->getCasValidationUrl()} timed out.");
             }
         }
 
         curl_close($curl);
-        $casAnswer = ob_get_contents();
+        $casAnswer = (string) ob_get_contents();
         ob_end_clean();
 
         // CAS answer on first line, CAS username (if any) on second line
@@ -145,13 +114,126 @@ class IuCasAuthentication
         return trim($result[0]) === "yes" ? trim($result[1]) : null;
     }
     
-    public function getLogoutUrl()
+    /**
+     *
+     * @param ?callable $onFailure On failure callback (passes current URL)
+     * @param ?callable $onSuccess On success callback (passes user name and current URL)
+     * @return mixed
+     */
+    public function authenticate(callable $onFailure = null, callable $onSuccess = null, callable $onLogin = null)
     {
-        $url = getenv('CAS_LOGOUT_URL');
-        return $url ? $url : 'https://cas.iu.edu/cas/logout';
+        $onFailure = $onFailure ?? [$this, 'defaultOnFailure'];
+        $onSuccess = $onSuccess ?? [$this, 'defaultOnSuccess'];
+        $onLogin = $onLogin ?? [$this, 'defaultOnLogin'];
+        
+        $user = $this->getUserName();
+        if ($user === null && $this->getCasTicket()) { // Have been to CAS, have ticket
+            $user = $this->validate();
+        } else {
+            return call_user_func($onLogin); // Must redirect to CAS to authenticate; overridable for testing purposes
+        }
+        
+        if ($user) {
+            return call_user_func($onSuccess, $user, $this->getCurrentUrl());
+        }
+        
+        return call_user_func($onFailure, $this->getCurrentUrl());
     }
     
-    public function getCurrentUrl()
+    /**
+     * Clears the session variable set by this class, and sends a request to CAS to logout.
+     *
+     * @return bool Whether the CAS remote call was successful.
+     */
+    public function logout(?int $timeout = null) : bool
+    {
+        $this->setUserName(null);
+
+        $curl = curl_init();
+        $url = $this->getLogoutUrl();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout ?? $this->getTimeout());
+        ob_start();
+        $success = curl_exec($curl);
+
+        if ($errNum = curl_errno($curl)) {
+            if ($this->logger) {
+                $this->logger->critical(__CLASS__ . ': ' . curl_error($curl));
+                $this->logger->critical(__CLASS__ . ": Logout request to {$url} timed out.");
+            }
+        }
+
+        curl_close($curl);
+        ob_end_clean();
+        
+        return (bool) $success;
+    }
+    
+    // ---------------------
+    //   Default handlers
+    // ---------------------
+    
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function defaultOnFailure(string $url) : void
+    {
+        header("HTTP/1.1 401 Unauthorized");
+        exit;
+    }
+    
+    /**
+     * @codeCoverageIgnore
+     */
+    protected function defaultOnSuccess(string $userName, string $currentUrl) : void
+    {
+        $this->setUserName($userName);
+    }
+    
+    /**
+     * Redirects to CAS login
+     *
+     * @codeCoverageIgnore
+     */
+    protected function defaultOnLogin() : void
+    {
+        header('Location: ' . $this->getCasLoginUrl(), true, 303);
+        exit;
+    }
+    
+    // ---------------------
+    //   Getters and Setters
+    // ---------------------
+    
+    public function getCasLoginUrl() : string
+    {
+        $env = getenv('CAS_LOGIN_URL');
+        $url = $env ? $env : self::CAS_LOGIN_URL;
+        
+        return $url
+            . "?cassvc={$this->service}&casurl="
+            . rawurlencode($this->redirectUrl);
+    }
+    
+    public function getCasTicket() : string
+    {
+        return $_GET['casticket'] ?? '';
+    }
+    
+    public function getCasValidationUrl() : string
+    {
+        // Validation **will** fail if $_GET['casticket'] is empty.
+        // A CAS ticket may be used only once.
+        $base = getenv('CAS_VALIDATION_URL');
+        $base = $base ? $base : self::CAS_VALIDATION_URL ;
+        
+        return $base
+            . "?cassvc={$this->service}&casurl="
+            . rawurlencode($this->redirectUrl)
+            . "&casticket=" . $this->getCasTicket();
+    }
+    
+    public function getCurrentUrl() : string
     {
         $url = 'http';
         $isHttps = empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off' ? false : true;
@@ -167,103 +249,66 @@ class IuCasAuthentication
         return $url;
     }
     
-    public function getSessionVar()
+    public function setLogger($logger) : void
     {
-        $var = getenv('CAS_SESSION_VAR');
-        return $var ? $var : 'CAS_USER';
+        $this->logger = $logger;
     }
     
-    public function getUserName()
+    public function getLogoutUrl() : string
     {
-        if ($this->userName) {
-            return $this->userName;
-        }
-        
-        $var = $this->getSessionVar();
-        $this->userName = isset($_SESSION[$var]) ? $_SESSION[$var] : null;
-        return $this->userName;
-    }
-    
-    public function setUserName($name)
-    {
-        $this->userName = $name;
-        
-        $var = $this->getSessionVar();
-        $_SESSION[$var] = $name;
+        $env = getenv('CAS_LOGOUT_URL');
+        return  $env ? $env : self::CAS_LOGOUT_URL;
     }
     
     /**
+     * @return string $redirect URL that authentication should redirect to after authentication.
+     *        Must be the same during authentication and validation steps.
      *
-     * @param callable|string|bool $onFailure Any of a) a URL to redirect to and exit on failure,
-     *        b) a callable on failure (call passes current URL), c) a `bool` value. If a boolean is passed,
-     *        sets a "401 Unauthorized" header and exits if `true`. (Defaults to `true`.)
-     * @param callable|string|bool $onSuccess Any of a) a URL to redirect to and exit on success,
-     *        b) a callable on success (call passes user name and current URL), c) a `bool` value.
-     *        If a boolean is passed, sets a `$_SESSION` variable if `true`, and takes no action if `false`.
-     *        (Defaults to `true`.)
-     * @throws InvalidArgumentException
-     * @return mixed
      */
-    public function authenticate($onFailure = true, $onSuccess = true)
+    public function getRedirectUrl() : string
     {
-        $success = false;
-        if ($this->getUserName()) {
-            $success = true;
-        } elseif ($this->getCasTicket()) { // Have been to CAS, have ticket
-            $success = $this->validate();
-        } else { // Must go to CAS to authenticate
-            header('Location: ' . $this->getCasLoginUrl(), true, 303);
-            $this->doExit();
-            return; // In case doExit() has been overridden
-        }
-        
-        if ($success) {
-            //$this->userName = $success;
-            if (is_bool($onSuccess)) {
-                if ($onSuccess) {
-                    $this->setUserName($success); // Also sets session variable
-                }
-                return;
-            } elseif (is_callable($onSuccess)) {
-                return call_user_func($onSuccess, $this->userName, $this->getCurrentUrl());
-            } elseif (is_string($onSuccess)) {
-                header('Location: '.$onSuccess, true, 303);
-                $this->doExit();
-                return; // In case doExit() has been overridden
-            }
-            throw new InvalidArgumentException(__CLASS__.'#authenticate() received a malformed onSuccess parameter');
-        }
-        
-        // i.e., not $success
-        if (is_bool($onFailure)) {
-            header("HTTP/1.1 401 Unauthorized");
-            if ($onFailure) {
-                $this->doExit();
-            }
-            return;
-        } elseif (is_callable($onFailure)) {
-            return call_user_func($onFailure, $this->getCurrentUrl());
-        } elseif (is_string($onFailure)) {
-            header("HTTP/1.1 401 Unauthorized");
-            header('Location: '.$onFailure, true, 303);
-            $this->doExit();
-            return; // In case doExit() has been overridden
-        }
-        throw new InvalidArgumentException(__CLASS__.'#authenticate() received a malformed onFailure parameter');
+        return $this->redirectUrl;
     }
     
-    public function logout()
+    /**
+     * @param string $url URL that authentication should redirect to after authentication.
+     *        Must be the same during authentication and validation steps.
+     *
+     */
+    public function setRedirectUrl(string $url) : void
     {
-        $this->setUserName(null);
+        $this->redirectUrl = $url;
     }
     
-    public function doExit() // In php5.6 may not be named `exit()`?!
+    public function getService() : string
     {
-        call_user_func($this->exitCallable);
+        return $this->service;
     }
     
-    public function setExit(callable $callable)
+    public function getSessionVar() : string
     {
-        $this->exitCallable = $callable;
+        $env = getenv('CAS_SESSION_VAR');
+        return $env ? $env : self::CAS_SESSION_VAR;
+    }
+    
+    public function getTimeout() : int
+    {
+        $env = getenv('CAS_TIMEOUT');
+        return $env ? (int) $env : self::CAS_DEFAULT_TIMEOUT;
+    }
+    
+    public function getUserName() : ?string
+    {
+        return $this->userName ?? $_SESSION[ $this->getSessionVar() ] ?? null;
+    }
+    
+    public function setUserName(?string $name) : void
+    {
+        $this->userName = $name;
+        if ($name === null) {
+            unset($_SESSION[ $this->getSessionVar() ]);
+        } else {
+            $_SESSION[ $this->getSessionVar() ] = $name;
+        }
     }
 }
